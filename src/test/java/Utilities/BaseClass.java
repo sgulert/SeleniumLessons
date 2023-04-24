a45a10d5defd6301a3f845e6136624b52aaf189e
sumeyye guler tuzuner <gulertuzuner@gmail.com>
2023-04-24 10:07:17 +0300

    baseclass is created for jUnit

---
+++ src/test/java/Utilities/BaseClass.java
@@ -0,0 +1,29 @@
+package Utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseClass {
+
+    protected WebDriver driver;
+    @Before
+
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @After
+    public void tearDown(){
+        //driver.quit();
+    }
+
+}
+
